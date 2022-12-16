34629a9edadbc93d4d5d206bf3496bd1aa14ef3d
Rodrigo Takashi Condo <rodrigo0takashi@gmail.com>
2022-12-16 16:39:45 -0300

    feat: configuração inicial ts + express

---
+++ backend/src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+
+class App {
+  public app: express.Express;
+
+  constructor () {
+    this.app = express();
+
+    this.config();
+
+  }
+
+  private config(): void {
+    this.app.use(express.json());
+  }
+
+  public start(PORT: string | number):void {
+    this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+  }
+}
+
+export { App };
+
+export const { app } = new App();
